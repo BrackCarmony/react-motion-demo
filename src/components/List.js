5a7d80f89321d1dcf9ddcd7d442d7e97796c424e
Kedirech <brackcarmony@gmail.com>
2017-12-12 20:59:04 -0700

    Cleanup to get starting where we want it to be.

--- src/components/List.js
+++ src/components/List.js
@@ -5,54 +5,25 @@ import {TransitionMotion, spring} from 'react-motion';
 class List extends Component{
   constructor(props){
     super(props);
+
+    // Start with some dummy data so that we don't have to add stuff each time.
     this.state = {
-      todos:[{id:1, todo:'Take out trash', complete:false}, {id:2, todo:'Walk the dog', complete:true}, {id:3, todo:'Walk the cat', complete:true}, {id:4, todo:'Walk the penguine', complete:true}],
+      todos:[
+        {id:1, todo:'Take out trash', complete:false},
+        {id:2, todo:'Walk the dog', complete:true},
+        {id:3, todo:'Walk the cat', complete:true},
+        {id:4, todo:'Walk the penguine', complete:true}
+      ],
       newTodo:''
     }
-  }
-
-  remove(todo){
-    console.log(todo);
-    this.setState({todos:this.state.todos.filter(e=>e!==todo)})
   }
 
   textChange(todo){
     this.setState({newTodo:todo})
   }
 
-
-  // I wish he had called these initial styles instead of defauly styles.
-  // What is that starting case for each of these elements.
-  getDefaultStlyes(){
-    // Here we are going to return a style object for each todo in our list.
-    // We could apply some other style properties here if we wanted to change
-    // other properties starting values based on something
-    return this.state.todos.map((e)=>({key:e.id+'key',data:e,  style:{'height':0,  opacity:1}}));
-  }
-
-  // The goals of each element.  We can use the spring helper function to
-  // set a target for each property.  We can also optionally pass in extra
-  // information about how the spring should operate.
-  // In this case we will start with all elements having the same goal.
-  getStyles(){
-    return this.state.todos.map((e)=>({key:e.id+'key',data:e, style:{height:spring(90), opacity:spring(1)}}));
-  }
-
-  willEnter() {
-    return {
-      height: 0,
-      opacity: 0,
-    };
-  };
-
-  willLeave() {
-    return {
-      height: spring(0),
-      opacity: spring(0)
-    };
-  };
-
-  add(){
+  add(event){
+    event.preventDefault();
     if (this.state.newTodo){
       let newId = this.state.todos.reduce((newId, e)=>{
         if(e.id>=newId) return e.id+1;
@@ -65,33 +36,21 @@ class List extends Component{
     }
   }
 
-  listAll(){
+  remove(todo){
+    this.setState({todos:this.state.todos.filter(e=>e!==todo)})
+  }
 
-    return(
-    <div>
-    How About this?
-      <TransitionMotion
-        defaultStyles={this.getDefaultStlyes()}
-        styles={this.getStyles()}
-        willEnter={this.willEnter}
-        willLeave={this.willLeave}
-      >{
-        styles=>(
-          <div className="List">
-            {styles.map((e)=>{
-              console.log(e.data);
-            return(
-                <div key={e.key} className="motionItem" style={e.style} >
-                  {e.data.todo} <span class="delete" onClick={()=>this.remove(e.data)}>X</span>
-                </div>
-              )
-            })
-            }
-          </div>
-        )
-      }
-      </TransitionMotion>
-    </div>
+  listAll(){
+    return (
+      <div className="List">
+        {this.state.todos.map((e)=>{
+        return (
+            <div key={e.key} className="motionItem" style={e.style} >
+              {e.todo} <span class="delete" onClick={()=>this.remove(e)}>X</span>
+            </div>
+          )
+        })}
+      </div>
     )
   }
 
@@ -100,12 +59,11 @@ class List extends Component{
     return (
       <div>
         <h1>My things to do</h1>
-        <input type="text" onChange={ input => this.textChange(input.target.value) } value={this.state.newTodo}/>
-        <button onClick={()=>this.add()}> New Todo</button>
+        <form onSubmit={(event)=>this.add(event)}>
+          <input type="text" onChange={ input => this.textChange(input.target.value) } value={this.state.newTodo}/>
+          <button type="submit"> New Todo</button>
+        </form>
         {this.listAll()}
-
-
-
       </div>
     )
   }
