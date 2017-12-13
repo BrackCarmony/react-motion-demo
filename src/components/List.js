fcab6f54856859b8c57effe6c4f8d57dbaaceb91
Kedirech <brackcarmony@gmail.com>
2017-12-12 20:21:02 -0700

    Motion Implemented.

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
-import {Motion, spring} from 'react-motion';
+import {TransitionMotion, spring} from 'react-motion';
 
 class List extends Component{
   constructor(props){
     super(props);
     this.state = {
-      todos:[{id:1, todo:'Take out trash', complete:false}, {id:2, todo:'Walk the dog', complete:true}],
+      todos:[{id:1, todo:'Take out trash', complete:false}, {id:2, todo:'Walk the dog', complete:true}, {id:3, todo:'Walk the cat', complete:true}, {id:4, todo:'Walk the penguine', complete:true}],
       newTodo:''
     }
   }
 
   remove(todo){
+    console.log(todo);
     this.setState({todos:this.state.todos.filter(e=>e!==todo)})
   }
 
@@ -23,12 +24,33 @@ class List extends Component{
   // I wish he had called these initial styles instead of defauly styles.
   // What is that starting case for each of these elements.
   getDefaultStlyes(){
-    return {styles:{height:0, opacity:1}};
+    // Here we are going to return a style object for each todo in our list.
+    // We could apply some other style properties here if we wanted to change
+    // other properties starting values based on something
+    return this.state.todos.map((e)=>({key:e.id+'key',data:e,  style:{'height':0,  opacity:1}}));
   }
 
+  // The goals of each element.  We can use the spring helper function to
+  // set a target for each property.  We can also optionally pass in extra
+  // information about how the spring should operate.
+  // In this case we will start with all elements having the same goal.
   getStyles(){
-    return {styles:{height:spring(60), opacity:spring(1)}};
+    return this.state.todos.map((e)=>({key:e.id+'key',data:e, style:{height:spring(90), opacity:spring(1)}}));
   }
+
+  willEnter() {
+    return {
+      height: 0,
+      opacity: 0,
+    };
+  };
+
+  willLeave() {
+    return {
+      height: spring(0),
+      opacity: spring(0)
+    };
+  };
 
   add(){
     if (this.state.newTodo){
@@ -45,11 +67,32 @@ class List extends Component{
 
   listAll(){
 
-    return this.state.todos.map(e=>(
-        <div key={e.id} className="todo">
-          <Todo  key={e.id} todo={e} remove={this.remove.bind(this, e)}/>
-        </div>
-    ))
+    return(
+    <div>
+    How About this?
+      <TransitionMotion
+        defaultStyles={this.getDefaultStlyes()}
+        styles={this.getStyles()}
+        willEnter={this.willEnter}
+        willLeave={this.willLeave}
+      >{
+        styles=>(
+          <div className="List">
+            {styles.map((e)=>{
+              console.log(e.data);
+            return(
+                <div key={e.key} className="motionItem" style={e.style} >
+                  {e.data.todo} <span class="delete" onClick={()=>this.remove(e.data)}>X</span>
+                </div>
+              )
+            })
+            }
+          </div>
+        )
+      }
+      </TransitionMotion>
+    </div>
+    )
   }
 
 
@@ -59,9 +102,8 @@ class List extends Component{
         <h1>My things to do</h1>
         <input type="text" onChange={ input => this.textChange(input.target.value) } value={this.state.newTodo}/>
         <button onClick={()=>this.add()}> New Todo</button>
-        <div className="List">
-          {this.listAll()}
-        </div>
+        {this.listAll()}
+
 
 
       </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function(props){
   return (
-    <div>
-      {props.todo.todo} <span>Checkmark</span> <span onClick={props.remove}>X</span> {props.todo.id}
+    <div className="Todo">
+
     </div>
   )
 }
